d182438d6690fa7a5342598f308fabdbc12ff799
Farhan Chauhan <farhan687@gmail.com>
2016-03-23 03:41:32 +0530

    Change docs

---
+++ docbase-config.js
@@ -0,0 +1,25 @@
+var docbaseConfig = {
+  "method": "github",
+  "map": {
+    "file": "map.json",
+    "path": ""
+  },
+  "generic": {
+    "baseurl": "",
+    "path": ""
+  },
+  "file": {
+    "path": ""
+  },
+  "github": {
+    "user": "farhan687",
+    "repo": "airbnb",
+    "path": "src",
+    "branch": "master",
+    "access_token": "945751d3f595b888024dbcab62b339134d2cac9f"
+  },
+  "indexHtml": "./html/main.html",
+  "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
+  "html5mode": false,
+  "versions": {}
+};
